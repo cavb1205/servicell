4ffe04f144455ac0c47c000e3cea207526d91bf6
camilo <cavb.1205@hotmail.com>
2020-03-30 22:05:18 -0300

    inicio del proyecto

---
+++ almacenes/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Almacen(models.Model):
+    nombre = models.CharField(max_length=255)
+    direccion = models.CharField(max_length=255)
+    ciudad = models.CharField(max_length=255)
+    capital = models.DecimalField(max_digits=10, decimal_places=2)
+    responsable = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+        
